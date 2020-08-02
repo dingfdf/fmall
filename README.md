4c674bc44adf193787d73592204c09d7972fa296
dingfan <dingfanim@163.com>
2020-08-02 16:39:58 +0800

    2020.8.2

--- README.md
+++ README.md
@@ -1,41 +1,41 @@
-# fmall
-# fmall-user用户服务8080
+# fmall  
+# fmall-user用户服务8080  
 
-用户模块
-http://user.fmall.com:8080/
-gmall-user-service用户服务的service层8070
-gmall-user-web用户服务的web层8080
+用户模块  
+http://user.fmall.com:8080/  
+gmall-user-service用户服务的service层8070  
+gmall-user-web用户服务的web层8080  
 
-后台管理模块
-http://manage.fmall.com:8080/
-fmall-manage-service用户服务的service层8071
-fmall-manage-web用户服务的web层8081
+后台管理模块  
+http://manage.fmall.com:8080/  
+fmall-manage-service用户服务的service层8071  
+fmall-manage-web用户服务的web层8081  
 
-商品详情模块
-http://item.fmall.com:8082/
-fmall-item-service前台的商品详情服务 8072
-fmall-item-web前台的商品详情展示 8082
+商品详情模块  
+http://item.fmall.com:8082/  
+fmall-item-service前台的商品详情服务 8072  
+fmall-item-web前台的商品详情展示 8082  
 
-搜索模块
-http://search.fmall.com:8083/
-fmall-search-web 搜索服务的前台 8083
-fmall-search-service 搜索服务的后台 8073
+搜索模块  
+http://search.fmall.com:8083/  
+fmall-search-web 搜索服务的前台 8083  
+fmall-search-service 搜索服务的后台 8073  
 
-购物车模块
-http://cart.fmall.com:8082/
-fmall-cart-web 购物车的前台 8084
-fmall-cart-service 购物车的后台 8074
+购物车模块  
+http://cart.fmall.com:8082/  
+fmall-cart-web 购物车的前台 8084  
+fmall-cart-service 购物车的后台 8074  
 
-用户认证中心模块
-http://passport.fmall.com:8085/
-fmall-passport-web 用户认证中心 8085
-fmall-user-service 用户服务的service层8070
+用户认证中心模块  
+http://passport.fmall.com:8085/  
+fmall-passport-web 用户认证中心 8085  
+fmall-user-service 用户服务的service层8070  
 
-订单模块
-http://order.fmall.com:8086/
-fmall-order-web 订单 8086
-fmall-order-service 订单服务 8076
+订单模块  
+http://order.fmall.com:8086/  
+fmall-order-web 订单 8086  
+fmall-order-service 订单服务 8076  
 
-支付模块
-http://payment.fmall.com:8087/
-fmall-payment 8087
+支付模块  
+http://payment.fmall.com:8087/  
+fmall-payment 8087  
--- fmall-manage-service/src/main/java/indi/df/fmall/manage/service/impl/SkuServiceImpl.java
+++ fmall-manage-service/src/main/java/indi/df/fmall/manage/service/impl/SkuServiceImpl.java
@@ -1,6 +1,7 @@
 package indi.df.fmall.manage.service.impl;
 
 import com.alibaba.dubbo.config.annotation.Service;
+import com.alibaba.fastjson.JSON;
 import indi.df.fmall.bean.PmsSkuAttrValue;
 import indi.df.fmall.bean.PmsSkuImage;
 import indi.df.fmall.bean.PmsSkuInfo;
@@ -10,10 +11,14 @@ import indi.df.fmall.manage.mapper.PmsSkuImageMapper;
 import indi.df.fmall.manage.mapper.PmsSkuInfoMapper;
 import indi.df.fmall.manage.mapper.PmsSkuSaleAttrValueMapper;
 import indi.df.fmall.service.SkuService;
+import indi.df.fmall.util.RedisUtil;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import redis.clients.jedis.Jedis;
+
 import java.math.BigDecimal;
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class SkuServiceImpl implements SkuService {
@@ -29,8 +34,8 @@ public class SkuServiceImpl implements SkuService {
     @Autowired
     PmsSkuImageMapper pmsSkuImageMapper;
 
-    //@Autowired
-    //RedisUtil redisUtil;
+    @Autowired
+    RedisUtil redisUtil;
 
     @Override
     public void saveSkuInfo(PmsSkuInfo pmsSkuInfo) {
@@ -59,7 +64,7 @@ public class SkuServiceImpl implements SkuService {
     }
 
     //使用数据库查询sku，早期版本，不用了，使用缓存查询
-    public PmsSkuInfo getSkuByIdFromDB(String skuId) {
+    public PmsSkuInfo getSkuByIdFromDb(String skuId) {
         // sku商品对象
         PmsSkuInfo pmsSkuInfo = new PmsSkuInfo();
         pmsSkuInfo.setId(skuId);
@@ -72,17 +77,47 @@ public class SkuServiceImpl implements SkuService {
     }
 
     //使用缓存查询sku
+    //测试的时候，如果单机可以同时打开两个页面发出请求
     @Override
     public PmsSkuInfo getSkuById(String skuId, String ip) {
         PmsSkuInfo pmsSkuInfo = new PmsSkuInfo();
         // 链接缓存
-
+        Jedis jedis = redisUtil.getJedis();
         // 查询缓存
-
-        // 如果缓存中没有，查询mysql
-
-        // mysql查询结果存入redis
-
+        String skuKey = "sku:" + skuId + ":info";
+        String skuJson = jedis.get(skuKey);
+        // 如果缓存中查得到，进行处理
+        if(StringUtils.isNotBlank(skuJson)){
+            //将查询到的json字符串转成sku对象
+            pmsSkuInfo = JSON.parseObject(skuJson, PmsSkuInfo.class);
+        }else{
+            // 如果缓存中没有，查询mysql
+            // 设置分布式锁,解决缓存击穿的问题
+            String token = UUID.randomUUID().toString(); //UUID.randomUUID().toString()是javaJDK提供的一个自动生成主键的方法
+            String OK = jedis.set("sku:" + skuId + ":lock", token, "nx", "px", 10*1000);// 拿到锁的线程有10秒的过期时间
+            // 设置成功，有权在10秒的过期时间内访问数据库,其他的请求（线程）走到这就被锁住，必须等过期时间结束
+            if(StringUtils.isNotBlank(OK)&&OK.equals("OK")){
+                pmsSkuInfo =  getSkuByIdFromDb(skuId);
+                if(pmsSkuInfo != null){
+                    // mysql查询结果存入redis
+                    jedis.set("sku:" + skuId + ":info", JSON.toJSONString(pmsSkuInfo));
+                }else{
+                    // 数据库中不存在该sku
+                    // 为了防止缓存穿透，将null或者空字符串值设置给redis，并设置3分钟的过期时间
+                    jedis.setex("sku:" + skuId + ":info", 60*3, JSON.toJSONString(""));
+                }
+                // 在访问mysql后，将mysql的分布锁释放
+                String lockToken = jedis.get("sku:" + skuId + ":lock");
+                if(StringUtils.isNotBlank(lockToken) && lockToken.equals(token)){ //如果当前的token与访问前设置的token相同
+                    //jedis.eval("lua");可与用lua脚本，在查询到key的同时删除该key，防止高并发下的意外的发生
+                    jedis.del("sku:" + skuId + ":lock");// 用token确认删除的是自己的sku的锁，防止删错别人的锁
+                }
+            }else{
+                // 设置失败，自旋（该线程在睡眠几秒后，重新尝试访问本方法）
+                return getSkuById(skuId,ip);
+            }
+        }
+        jedis.close();
         return pmsSkuInfo;
     }
 
